db0df35b6e48c9d8a13ff35808c1755660de6d01
Aizhan-px <aizhan.mysakanova.ch@gmail.com>
2026-03-23 19:02:50 +0100

    update

--- src/s2-homeworks/hw08/User.tsx
+++ src/s2-homeworks/hw08/User.tsx
@@ -2,18 +2,37 @@ import React from 'react'
 import { UserType } from './HW8'
 import s from "./HW8.module.css"
 
-// types
+
 type UserPropsType = {
     u: UserType
 }
 
 const User: React.FC<UserPropsType> = ({ u }) => {
     return (
-        <tr id={'hw8-user-'+ u._id} className={s.item}>
-            <td id={'hw8-user-name-'+ u._id}  className={s.nameCol}>{u.name}</td>
-            <td id={'hw8-user-age-'+ u._id}>{u.age}</td>
+        <tr  className={s.item}>
+            <td id={'hw8-user-name-' + u._id} className={s.nameCol}>
+               {u.name}
+
+            </td>
+            <td id={'hw8-user-age-' + u._id}>
+                {u.age}
+            </td>
         </tr>
     )
 }
+//id={'hw8-user-' + u._id + '-' + u.age}
 
 export default User
+
+
+
+//return (
+//         <tr id={'hw8-user-'+ u._id} >
+//             <td id={'hw8-user-name-'+ u._id}>
+//             {u.name}
+//             </td>
+//             <td id={'hw8-user-age-'+ u._id}>
+//             {u.age}
+//             </td>
+//         </tr>
+//     )
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -7,7 +7,7 @@ type ActionType =
 export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': {
-            // возвращаем новый массив, сортируем по имени
+
             return [...state].sort((a, b) =>
                 action.payload === 'up'
                     ? a.name.localeCompare(b.name)
@@ -15,7 +15,6 @@ export const homeWorkReducer = (state: UserType[], action: ActionType): UserType
             )
         }
         case 'check': {
-            // фильтруем только людей с возрастом >= payload
             return state.filter(user => user.age >= action.payload)
         }
         default:
