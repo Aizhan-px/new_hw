7c29d542b88c87837d521aa9144ebc7ba95531cb
Aizhan-px <aizhan.mysakanova.ch@gmail.com>
2026-04-04 12:49:17 +0200

    update

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -33,25 +33,22 @@
 
 import { UserType } from '../HW8'
 
-type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+type ActionType = { type: 'sort'; payload: 'up' | 'down' } | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (
-    state: UserType[],
-    action: ActionType
-): UserType[] => {
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
     switch (action.type) {
-        case 'sort':
-            return [...state].sort((a, b) =>
-                action.payload === 'up'
-                    ? a.name.localeCompare(b.name)
-                    : b.name.localeCompare(a.name)
-            )
-        case 'check':
-            return state.filter(user => user.age >= action.payload)
-        default:
-            return state
+        case 'sort': {
+            const newState = [...state].sort((a, b) => {
+                if (a.name > b.name) return 1
+                else if (a.name < b.name) return -1
+                else return 0
+            })
+            return action.payload === 'up' ? newState : newState.reverse()
+    }
+    case 'check': {
+            return state.filter(a => a.age >= action.payload)
+        }
+        default: return state
     }
 }
 
