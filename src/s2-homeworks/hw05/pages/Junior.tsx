46ec2880afa15e4dbb5c620c1f7c7f5c998fc683
Aizhan-px <aizhan.mysakanova.ch@gmail.com>
2026-04-18 17:21:58 +0200

    update

--- src/s2-homeworks/hw05/pages/Junior.tsx
+++ src/s2-homeworks/hw05/pages/Junior.tsx
@@ -7,9 +7,9 @@ import HW9 from '../../hw09/HW9'
 export default function Junior() {
     return (
         <div id={'hw5-page-junior'}>
-            {/*<HW6/>*/}
-            {/*<HW7 />*/}
-            {/*<HW8 />*/}
+            <HW6/>
+            <HW7 />
+            <HW8 />
             <HW9 />
         </div>
     )
