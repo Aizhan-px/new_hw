93e69c25fd01cc225dfb780e14e2f0f78c44775d
Aizhan-px <aizhan.mysakanova.ch@gmail.com>
2026-05-05 22:10:06 +0200

    update

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -17,6 +17,9 @@ function HW11() {
 
     const change = (event: any, value: any) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+        if (value) {
+        setValue1(value)
+        } else { setValue2(value) }
     }
 
     return (
